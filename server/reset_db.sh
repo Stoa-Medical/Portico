53beb9b6d0d7fa80b0724a8112a384edddee14cf
Eric Pan <epan@stoamedical.io>
2025-05-08 01:58:31 -0700

    Fix path in reset script

--- server/reset_db.sh
+++ server/reset_db.sh
@@ -4,4 +4,4 @@ atlas schema apply \
   --url "postgresql://postgres:postgres@localhost:54322/postgres?search_path=public&sslmode=disable" \
   --to "file://scheme.hcl" \
   --auto-approve
-psql postgresql://postgres:postgres@localhost:54322/postgres -f seed.sql
+psql postgresql://postgres:postgres@localhost:54322/postgres -f examples/seed.sql
